239913cee5a7695b708cd7cc85a754a4f23056de
samtetra <samtetra07@gmail.com>
2026-02-17 21:06:49 +0530

    Add initial Jenkins CI pipeline greeting script

---
+++ app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("===================================")
+print(" Hello from Jenkins CI Pipeline 🚀 ")
+print(" Build is working successfully!   ")
+print("===================================")
